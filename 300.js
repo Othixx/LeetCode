f212083e4f133a9b673743ce07fcf50ae227fe3a
Othixx <othixx@qq.com>
2025-01-18 14:45:09 +0800

    update 300.js

--- 300.js
+++ 300.js
@@ -2,20 +2,25 @@
  * @param {number[]} nums
  * @return {number}
  */
-var lengthOfLIS = function(nums) {
-    let max = 1;
-    let dp = new Array(nums.length).fill(1);
-    let dfs = function(i){
-        for(let j = 0; j < i; j++){
-            if(nums[j] < nums[i] && dp[j] + 1 > dp[i]) dp[i] = dp[j] + 1;
+var lengthOfLIS = function (nums) {
+    let p = [];
+    p.push(nums[0]);
+    // 查找第一个下标j，让p[j]>=nums[i]
+    for (let i = 1; i < nums.length; i++) {
+        // 二分查找
+        let m = 0;
+        let n = p.length - 1;
+        let pos = Math.floor((m + n) / 2);
+        while (m <= n) {
+            if (p[pos] >= nums[i]) n = pos - 1;
+            else m = pos + 1;
+            pos = Math.floor((m + n) / 2);
         }
-    };
-    for(let i = 0; i < nums.length; i++){
-        dfs(i);
-        if(dp[i] > max) max = dp[i];
+        if (m >= p.length) p.push(nums[i]);
+        else p[m] = nums[i];
     }
-    return max;
+    return p.length;
 };
 
-nums = [1,3,6,7,9,4,10,5,6];
-lengthOfLIS(nums);
+let nums = [10, 9, 2, 5, 3, 7, 101, 18];
+console.log(lengthOfLIS(nums));
